21fe32c2e22fcbca3f80641b63ed397282e1830a
Pranjal Rai <pranjalbhs@gmail.com>
2021-05-11 20:33:35 +0530

    twelveth-commit

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,18 +6,18 @@ function Welcome() {
   return (
     <div className="App container-fluid">
    <div className="row welcome-screen-header">
-     <div className="col-md-4"></div>
-   <div className="col-md-5"><h1> Welcome to the Dice Game</h1></div>
+     <div className="col-md-3"></div>
+   <div className="col-md-6 welcome-screen-title"><h1>Welcome to the Dice Game</h1></div>
    <div className="col-md-3"></div>
    </div>
    <div className="row row2">
-   <div className="col-md-4"></div>
-   <div className="col-md-6"><img className="dice-image" src="./dice-image.png" /></div>
-   <div className="col-md-2"></div>
+   <div className="col-md-3"></div>
+   <div className="col-md-6 welcome-img-section"><img className="dice-image" src="./dice-image.png" /></div>
+   <div className="col-md-3"></div>
    </div>
    <div className="row row3">
-   <div className="col-md-5"></div>
-   <div className="col-md-3"><Link to="/addplayers"><button className="start-button"> Start Playing</button></Link></div>
+   <div className="col-md-4"></div>
+   <div className="col-md-4 welcome-screen-button-section"><Link to="/addplayers"><button className="start-button"> Start Playing</button></Link></div>
    <div className="col-md-4"></div>   
    </div>
     </div>
